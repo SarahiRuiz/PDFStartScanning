b123b3559fe9bd54b231713ad78870a14c4f3b85
anaruizsolera <ana.ruiz@solera.com>
2023-11-06 14:01:35 -0600

    Adding new method to click on

--- PDFTest/PageObject/GenericsMethod.cs
+++ PDFTest/PageObject/GenericsMethod.cs
@@ -6,21 +6,6 @@ namespace PDFTesT.PageObject
 {
     public class GenericsMethod : SetUp
     {       
-        public void EnterTextAndValidate(IWebElement element, string value)
-        {
-            Assert.IsTrue(element.Displayed, $"Element input is {value} entered.");
-            element.SendKeys(value);
-            Thread.Sleep(5000);
-            string textInput = element.GetAttribute("value");
-            Assert.IsTrue(textInput.Equals(value));
-        }
-
-        public void ClickOnRadioButton(IWebElement element)
-        {
-            Assert.IsTrue(element.Displayed, $"Element radio button was {element} entered.");
-            element.Click();
-        }
-
         public IWebElement CreateDynamicElement(string element, string dynamicValue)
         {
             string dynamicElement = element.Replace("?", dynamicValue);
@@ -52,11 +37,23 @@ namespace PDFTesT.PageObject
         public bool ClickOn(IWebElement element, int timeOut = 10)
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOut));
-            return wait.Until(d =>
+            return wait.Until(d => WaitElementClicked(wait, element));
+        }
+
+        public bool WaitElementClicked(WebDriverWait wait, IWebElement element)
+        {
+            try
             {
-                element.Click();
-                return true;
-            });
+                return wait.Until(d =>
+                {
+                    element.Click();
+                    return true;
+                });
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
