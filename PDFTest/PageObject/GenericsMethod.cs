dfb488260c442a2cb025f18578d0c9e0768a05e6
anaruizsolera <ana.ruiz@solera.com>
2023-11-07 11:20:25 -0600

    Adding wait for element

--- PDFTest/PageObject/GenericsMethod.cs
+++ PDFTest/PageObject/GenericsMethod.cs
@@ -1,6 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using PDFTest.drivers;
+using System.Text;
 
 namespace PDFTesT.PageObject
 {
@@ -11,6 +12,30 @@ namespace PDFTesT.PageObject
             string dynamicElement = element.Replace("?", dynamicValue);
             return driver.FindElement(By.XPath(dynamicElement));
         }
+        /*public IWebElement CreateDynamicElement(string element, string dynamicValue1, string dynamicValue2)
+        {
+            string dynamicElement = "";
+            try
+            {
+                int totalIncognits = element.Count(c => c == '?');
+                if (totalIncognits==2)
+                {
+                    dynamicElement = element.Replace("?", dynamicValue1);
+                }else if(totalIncognits == 1)
+                {
+                    dynamicElement = element.Replace("?", dynamicValue2);
+                }
+                else if(totalIncognits<=0)
+                {
+                    return driver.FindElement(By.XPath(dynamicElement));
+                }               
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return null;
+        }*/
 
         public bool EnterText(IWebElement element, string value, int timeOut = 10)
         {
@@ -48,6 +73,26 @@ namespace PDFTesT.PageObject
                 {
                     element.Click();
                     return true;
+                });
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool WaitForElement(IWebElement element, int timeOut = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOut));
+            return wait.Until(d => WaitElementVisibility(wait, element));
+        }
+
+        private bool WaitElementVisibility(WebDriverWait wait, IWebElement element)
+        {
+            try
+            {
+                return wait.Until(d =>
+                {
+                    return element.Displayed;
                 });
             }
             catch (Exception ex)
